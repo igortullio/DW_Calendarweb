7fb5746145b0f4ff2f888b70e1cf22830ac34989
Pedro Paul <ppaul804@gmail.com>
2016-10-10 00:33:49 -0300

    Usando django.shortcut em views.py de login

--- login/views.py
+++ login/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import usuario
 
 def index(request):
 	all_usuario = usuario.objects.all()
-	template = loader.get_template('login/index.html')
-	context = {
-		'all_usuario': all_usuario,		
-	}
-	return HttpResponse(template.render(context, request))
+	context = {'all_usuario': all_usuario}
+	return render(request, 'login/index.html', context)
 	
 def detail(request, usuario_id):
 	return HttpResponse("<h2>Detalhes do usuario " + str(usuario_id) + "</h2>")
