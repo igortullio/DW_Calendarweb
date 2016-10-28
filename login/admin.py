c05d6c618c4e850f086a6010b2e6280b8f1b75bd
Pedro Paul <ppaul804@gmail.com>
2016-10-27 23:28:23 -0200

    Remoção de arquivos

--- login/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import calendario
-from .models import usuario
-from .models import evento
-
-admin.site.register(calendario)
-admin.site.register(usuario)
-admin.site.register(evento)
