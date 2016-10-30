3759bd964ffefbd2f61c578c731e76e989908cbf
Pedro Paul <ppaul804@gmail.com>
2016-10-30 21:02:34 -0200

    Adição do botão de retorno ao inicio da página

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,55 +1,53 @@
 /*=============================================================
-    Authour URI: www.binarytheme.com
-    License: Commons Attribution 3.0
-
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
+ Authour URI: www.binarytheme.com
+ License: Commons Attribution 3.0
+ 
+ http://creativecommons.org/licenses/by/3.0/
+ 
+ 100% To use For Personal And Commercial Use.
+ IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
+ 
+ ========================================================  */
 
 
 (function ($) {
     "use strict";
     var mainApp = {
-
         main_fun: function () {
             /*====================================
              CUSTOM LINKS SCROLLING FUNCTION 
-            ======================================*/
+             ======================================*/
 
             $('a[href*=#]').click(function () {
                 if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-               && location.hostname == this.hostname) {
+                        && location.hostname == this.hostname) {
                     var $target = $(this.hash);
                     $target = $target.length && $target
-                    || $('[name=' + this.hash.slice(1) + ']');
+                            || $('[name=' + this.hash.slice(1) + ']');
                     if ($target.length) {
                         var targetOffset = $target.offset().top;
                         $('html,body')
-                        .animate({ scrollTop: targetOffset }, 800); //set scroll speed here
+                                .animate({scrollTop: targetOffset}, 800); //set scroll speed here
                         return false;
                     }
                 }
             });
-          
 
-          
+
+
             /*====================================
-               WRITE YOUR SCRIPTS BELOW 
-           ======================================*/
+             WRITE YOUR SCRIPTS BELOW 
+             ======================================*/
 
-
-          
-        },
-
+            
+            
+        },//fim main_fun
         initialization: function () {
             mainApp.main_fun();
 
         }
 
-    }
+    };
     // Initializing ///
 
     $(document).ready(function () {
@@ -57,6 +55,3 @@
     });
 
 }(jQuery));
-
-
-
